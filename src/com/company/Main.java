e1a0dddd33b85e22848ccf1525a47cada15ae798
Oleksii Kutnii <okutnii@griddynamics.com>
2021-11-02 13:33:35 +0200

    commit with some changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
         // some code...
+        // another change
     }
 }
